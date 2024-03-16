1ada8dde28cec3a5d042112039c477c566b540e1
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-03-16 00:57:01 -0400

    Styled Login component
    Styled Login component

--- src/component/Login.js
+++ src/component/Login.js
@@ -5,6 +5,8 @@ import validate from '../utils/validate';
 import { auth } from '../utils/firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import img2 from '../ASL-cover-image.jpg'
+import img3 from '../three-hands-sign-the-word-new.jpg'
 
 const Login = () => {
 
@@ -104,32 +106,39 @@ const Login = () => {
       <LoginHeader />
 
 
+      <div className="absolute">
+                {/* <img src='https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg' */}
+                <img src = {img2}
+                     alt='Background' />
+      </div>
+
+
       <form onSubmit={(e) => e.preventDefault()}
-      className="w-3/12 absolute p-16 bg-black bg-opacity-60 my-36 mx-auto right-0 left-0 text-white rounded-lg">
+      className="w-3/12 absolute p-16 bg-orange-300 bg-opacity-60 my-36 mx-auto right-0 left-0 text-white rounded-lg">
 
         {/* Sign In form */}
-        <h1 className="font-bold text-3xl my-2 p-3">{isSignInForm ? "Sign In": "Sign Up" }</h1>
+        <h1 className="font-bold text-4xl my-2 p-3">{isSignInForm ? "Sign In": "Sign Up" }</h1>
 
        { !isSignInForm && 
         <input
         ref = {name}
-        type="text" placeholder="Name" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
+        type="text" placeholder="Name" className="p-4 my-4 w-full bg-white rounded-lg  bg-opacity-80 text-black font-semibold" />
        }
 
         <input 
         ref = {email}
-        type="text" placeholder="Email-address" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
+        type="text" placeholder="Email-address" className="p-4 my-4 w-full bg-white rounded-lg bg-opacity-80 text-black font-semibold" />
         
         <input
         ref = {password}
-         type="password" placeholder="Password" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
+         type="password" placeholder="Password" className="p-4 my-4 w-full bg-white rounded-lg bg-opacity-80 text-black font-semibold" />
 
         
         {/* To display error messages after data validations in form */}
         <p className="text-red-500 font-semibold py-3 ">{errorMessage}</p>
 
 
-        <button className="p-4 my-6 bg-amber-500 rounded-lg font-semibold w-full hover:bg-amber-400"
+        <button className="p-4 my-6 bg-amber-500 rounded-lg font-semibold w-full hover:bg-amber-400 text-lg"
          onClick={handleButtonClick}
            >
              {isSignInForm ? "Sign In": "Sign Up" } </button>
@@ -138,7 +147,7 @@ const Login = () => {
 
         {/* SIGN UP FORM  */}
 
-        <p className="font-semibold cursor-pointer" onClick={toggleSignIn}>{isSignInForm ? "New to Sign Lingual? Sign Up Now!": "Already registered? Sign In now!" }</p>
+        <p className="font-semibold cursor-pointer text-lg" onClick={toggleSignIn}>{isSignInForm ? "New to Sign Lingual? Sign Up Now!": "Already registered? Sign In now!" }</p>
       </form>
     </div>
   )
--- src/component/LoginHeader.js
+++ src/component/LoginHeader.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import img2 from '../WhatsApp Image 2024-03-14 at 1.46.35 AM.jpeg'
 
 const LoginHeader = () => {
   return (
     <div>
-      <div className=" bg-amber-400 flex  justify-between   h-12">
-     <div className="  font-extrabold px-4">
-      Sign Language Converter
+      <div className=" bg-amber-400 flex bg-gradient-to-t from-orange-400  justify-between   h-20">
+      {/* <img src = {img2}
+                     alt='Background' /> */}
+     <div className="  font-extrabold px-5 p-4 text-3xl">
+      SignLingual
      </div>
      </div>
     </div>
