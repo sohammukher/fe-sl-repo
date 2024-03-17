d9ae425ee24d6f05469c5847aa69753271c9395d
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-03-17 03:06:17 -0400

    Updated style for cards
    Updated style for cards

--- src/component/DifficultyCard.js
+++ src/component/DifficultyCard.js
@@ -8,8 +8,8 @@ const DifficultyCard = (props) => {
 
 
   return (
-    <div className='p-6 justify-center bg-amber-300 bg-opacity-70 rounded-lg m-3 w-76 text-center'>
-        <div className=' text-3xl'>{difficultyLvl}</div>
+    <div className='p-6 justify-center bg-orange-300 bg-opacity-50 rounded-lg m-3 w-76 text-center'>
+        <div className=' text-3xl p-4'>{difficultyLvl}</div>
         <div className='flex    justify-center items-center'>
             {words.map((curr,index)=><Word word={curr}/>)}
         </div>
--- src/component/MenuType.js
+++ src/component/MenuType.js
@@ -4,8 +4,8 @@ const MenuType = (props) => {
     const { name, image } = props.props;
 
     return (
-        <div className='p-6 justify-center bg-amber-300 bg-opacity-70 rounded-lg m-3 w-76'>
-            <div className="text-center mb-4 text-4xl ">{name}</div>
+        <div className='p-6 justify-center bg-amber-400 bg-opacity-70 rounded-lg m-3 w-76 hover:bg-amber-200'>
+            <div className="text-center mb-4 text-4xl font-semibold ">{name}</div>
             <img src={image} alt="error loading...." className='w-full h-auto rounded-lg' />
         </div>
     );
