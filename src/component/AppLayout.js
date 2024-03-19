15cd6dfaf7bbd95061938599254bf1cd195b1385
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-03-19 02:21:29 -0400

    Removed unused imports
    Removed unused imports

--- src/component/AppLayout.js
+++ src/component/AppLayout.js
@@ -1,25 +1,13 @@
 import React from 'react'
 import Header from './Header'
-import { Outlet, useNavigate } from 'react-router-dom'
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../utils/firebase';
-import {useEffect} from "react"
-import { Provider, useDispatch } from 'react-redux';
-import { addUser, removeUser } from '../utils/userSlice'
+import { Outlet } from 'react-router-dom'
+import { Provider } from 'react-redux';
 import appStore from '../utils/appStore';
 
 
 const AppLayout = () => {
 
-  const theme = 0; // Change
-
-  // useEffect(()=>{
-  //   if(theme === "dark"){
-  //     document.documentElement.classList.add("dark");
-  //   }else{
-  //     document.documentElement.classList.add("dark");
-  //   }
-  // })
+  // const theme = 0; // Change
 
   
   
