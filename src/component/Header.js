81904fbf85c61a1de2983ee40b4681f259e2bf3c
anash3420 <anashshelat@gmail.com>
2024-03-20 01:22:42 -0400

    Changed UI Home Screen

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,10 @@
-///Header : Contains the navbar
-// Header:
-// -Logo
-// -Nav Items
+// ///Header : Contains the navbar
+// // Header:
+// // -Logo
+// // -Nav Items
 
-// -------------------------
-// import { useState,useEffect } from "react";
+// // -------------------------
+// // import { useState,useEffect } from "react";
 import { auth } from "../utils/firebase";
 import { signOut } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
@@ -13,100 +13,261 @@ import { domain } from "../utils/endpoints";
 import useOnlineStatus from "../utils/useOnlineStatus"
 
 
-const Header = () => {
-  // let loginBtnTxt = "Login";
+// const Header = () => {
+//   // let loginBtnTxt = "Login";
 
-//  let [modeText, setModeText] = useState("Dark");
+// //  let [modeText, setModeText] = useState("Dark");
 
-//   const [loginBtn, setloginBtn] = useState(loginBtnTxt);
-
-
-//   const [welcomeText,setWelcomeText] = useState("Welcome Visitor");
-
-let [modeText, setModeText] = useState("Dark");// Dark Mode
-
-function toggleTheme(){
-  // document.documentElement.classList.add('dark');
-  document.documentElement.classList.contains('dark')?document.documentElement.classList.remove('dark'):document.documentElement.classList.add('dark');
-  modeText === "Dark"? setModeText("Light"):setModeText("Dark");
+// //   const [loginBtn, setloginBtn] = useState(loginBtnTxt);
 
 
-}
+// //   const [welcomeText,setWelcomeText] = useState("Welcome Visitor");
 
-function isDarkModeActive(){
-  return document.documentElement.classList.contains('dark');
-}
+// let [modeText, setModeText] = useState("Dark");// Dark Mode
 
-// Internet Status:
-  // Checking Internet Status :
-  const internetStatus = useOnlineStatus()
-
-
-  const navigate = useNavigate();
-
-  // To sign out the user
-  const handleLogout = () => {
-
-    signOut(auth).then(() => {
-      // Sign-out successful.
-      navigate("/login");
-    }).catch((error) => {
-      // An error happened.
-      navigate("/error")
-    });
-  }
-
-  // // Internet Status
-  // console.log("internetStatus "+internetStatus)
-  // if(internetStatus === false){
-  //   return <h1>You are Offline!!!!</h1>
-  // }
+// function toggleTheme(){
+//   // document.documentElement.classList.add('dark');
+//   document.documentElement.classList.contains('dark')?document.documentElement.classList.remove('dark'):document.documentElement.classList.add('dark');
+//   modeText === "Dark"? setModeText("Light"):setModeText("Dark");
 
 
-  return (
+// }
 
-    <div className=" bg-amber-400 flex bg-gradient-to-t from-orange-400  justify-between   h-20 w-[100%]">
-    <Link to="/">
-     <div className=" font-extrabold px-5 p-4 text-3xl cursor-pointer">
-      SignLingual
-     </div>
-     </Link>
+// function isDarkModeActive(){
+//   return document.documentElement.classList.contains('dark');
+// }
 
-     <div className="px-5 py-6  font-bold text-xl">
-     <Link to="/">
-     <span className="px-2 cursor-pointer">HOME</span>
-     </Link>
+// // Internet Status:
+//   // Checking Internet Status :
+//   const internetStatus = useOnlineStatus()
+
+
+//   const navigate = useNavigate();
+
+//   // To sign out the user
+//   const handleLogout = () => {
+
+//     signOut(auth).then(() => {
+//       // Sign-out successful.
+//       navigate("/login");
+//     }).catch((error) => {
+//       // An error happened.
+//       navigate("/error")
+//     });
+//   }
+
+//   // // Internet Status
+//   // console.log("internetStatus "+internetStatus)
+//   // if(internetStatus === false){
+//   //   return <h1>You are Offline!!!!</h1>
+//   // }
+
+
+//   return (
+
+//     <div className=" bg-amber-400 flex bg-gradient-to-t from-orange-400  justify-between   h-20 w-[100%]">
+//     <Link to="/">
+//      <div className=" font-extrabold px-5 p-4 text-3xl cursor-pointer">
+//       SignLingual
+//      </div>
+//      </Link>
+
+//      <div className="px-5 py-6  font-bold text-xl">
+//      <Link to="/">
+//      <span className="px-2 cursor-pointer">HOME</span>
+//      </Link>
 
      
-     <span className="px-2 cursor-pointer">ABOUT</span>
+//      <span className="px-2 cursor-pointer">ABOUT</span>
 
-     <Link to='/contact'>
-     <span className="px-2 cursor-pointer">CONTACT</span>
-     </Link>
-
-
-     <Link to='/faq'>
-     <span className="px-2 cursor-pointer">FAQ</span>
-     </Link>
-
-     <span className="">Online Status: {internetStatus?"🟢":"🔴"}</span>
-
-     </div>
+//      <Link to='/contact'>
+//      <span className="px-2 cursor-pointer">CONTACT</span>
+//      </Link>
 
 
-{/* <div className="bg-blue-800 p-2 rounded-lg  text-white font-semibold"> Logout */}
-{/* </div> */}
+//      <Link to='/faq'>
+//      <span className="px-2 cursor-pointer">FAQ</span>
+//      </Link>
+
+//      <span className="">Online Status: {internetStatus?"🟢":"🔴"}</span>
+
+//      </div>
 
 
-<button onClick={toggleTheme} className=" font-bold p-3 m-3 ml-[40%] bg-black text-white rounded-xl hover:bg-blue-400 dark:hover:bg-blue-400 dark:bg-blue-50 dark:text-black toggle-dark">{modeText} Mode </button>
-
-<button onClick={handleLogout} className="bg-red-800 text-xl text-white rounded-xl p-3 m-3 hover:bg-red-600">Logout</button>
-
-    </div>
-  );
+// {/* <div className="bg-blue-800 p-2 rounded-lg  text-white font-semibold"> Logout */}
+// {/* </div> */}
 
 
+// <button onClick={toggleTheme} className=" font-bold p-3 m-3 ml-[40%] bg-black text-white rounded-xl hover:bg-blue-400 dark:hover:bg-blue-400 dark:bg-blue-50 dark:text-black toggle-dark">{modeText} Mode </button>
 
-};
+// <button onClick={handleLogout} className="bg-red-800 text-xl text-white rounded-xl p-3 m-3 hover:bg-red-600">Logout</button>
 
-export default Header;
+//     </div>
+//   );
+
+
+
+// };
+
+// export default Header;
+
+import * as React from "react"
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  MenuItem,
+} from "@mui/material"
+import { FiMenu } from "react-icons/fi"
+import { HiArrowSmRight } from "react-icons/hi"
+// mock data
+const links = [["Home", '/'],["About", '/about'], ["Docs", '/faq'], ["Contact Us", '/contact']]
+const Logo = () => (
+  <Box
+    sx={{
+      fontSize: "24px",
+      fontWeight: 800,
+      textDecoration: "none",
+      color: "black",
+    }}
+  >
+    ❍ SignLingual
+  </Box>
+)
+// simple nav
+export default function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null)
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget)
+  }
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  // let [modeText, setModeText] = useState("Dark");// Dark Mode
+
+  // function toggleTheme(){
+  //   // document.documentElement.classList.add('dark');
+  //   document.documentElement.classList.contains('dark')?document.documentElement.classList.remove('dark'):document.documentElement.classList.add('dark');
+  //   modeText === "Dark"? setModeText("Light"):setModeText("Dark");
+  // }
+
+  // function isDarkModeActive(){
+  //   return document.documentElement.classList.contains('dark');
+  // }
+  
+  // Internet Status:
+    // Checking Internet Status :
+    const internetStatus = useOnlineStatus()
+  
+    const navigate = useNavigate();
+  
+    // To sign out the user
+    const handleLogout = () => {
+  
+      signOut(auth).then(() => {
+        // Sign-out successful.
+        navigate("/login");
+      }).catch((error) => {
+        // An error happened.
+        navigate("/error")
+      });
+    }
+
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        bgcolor: "background.paper",
+        py: 1,
+      }}
+    >
+      <Container>
+        <Toolbar
+          variant="dense"
+          disableGutters
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Logo />
+          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 1 }}>
+            {links.map(link => (
+
+              <Link to={link[1]}>
+                <Button
+                  component="a"
+                  href="#"
+                  key={link[0]}
+                  onClick={handleCloseNavMenu}
+                  sx={{ fontWeight: 500 }}
+                >
+                  {link[0]}
+                </Button>
+              </Link>
+            ))}
+            <Link to="/login">
+            <Button
+              component="a"
+              href="#"
+              variant="contained"
+              color="primary"
+              sx={{
+                borderRadius: 999,
+              }}
+              endIcon={<HiArrowSmRight size={16} />}
+            >
+              Login
+            </Button>
+            </Link>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="medium"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+            >
+              <FiMenu />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+              disablePortal={true}
+            >
+              {links.map(link => (
+                <Link to={link[1]}>
+                  <MenuItem key={link} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{link[0]}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -1,60 +1,121 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, redirect } from 'react-router-dom';
 
 import { domain } from "../utils/endpoints";
 
 
 const Landing = () => {
   return (
-   
-
-    <div className="">
-
-    
-    {/* <div className='bg-white p-10 bg-black bg-gradient-to-r  bg-gradient-to-r from-black via-white to-black from-black to-white w-screen'> */}
-    <div className='p-10 bg-gradient-to-r from-black via-white to-black w-screen'>
+    <div className="" style={{
+      overflow: 'hidden',
+      margin: 0,
+      padding: 0,
+      height: '100vh',
+    }}>
+      {/* <div className='bg-white p-10 bg-black bg-gradient-to-r  bg-gradient-to-r from-black via-white to-black from-black to-white w-screen'> */}
+      {/* <div className='p-10 bg-gradient-to-r from-black via-white to-black w-screen'> */}
       {/* <img className='object-none justify-' src='https://i.pinimg.com/originals/87/86/7b/87867b9749f01842a84f90228a7147f8.jpg' alt='banner' /> */}
-    <h1 className="text-3xl font-bold text-center text-black">Welcome to SignLingual🖖🏻</h1>
+      {/* <h1 className="text-3xl font-bold text-center text-black">Welcome to SignLingual🖖🏻</h1>
     <p className="text-xl text-center font-semibold p-4 text-black">Break down communication barriers, Translate text into sign language instantly!</p>
-    </div>
+    </div> */}
 
+      {/* <div className="absolute ">
+        <img
+          className="h-screen object-cover w-screen opacity-30"
+          src="https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg"
+          alt="background"
+        />
+      </div> */}
 
+      {/* <div className="relative opacity-50"  style={{backgroundImage: "url(https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg)"}}> */}
 
-    <div className='absolute '>   
-    <img className='h-screen object-cover w-screen opacity-30' src='https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg' alt='background' />
-    </div>    
-
-    {/* <div className="relative opacity-50"  style={{backgroundImage: "url(https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg)"}}> */}
-    
-    {/* <img className='relative opacity-20' src='https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg' /> */}
-    
-
-
-    <div className='flex justify-center items-center h-screen'>
-      <div className='relative opacity-100 p-32 flex flex-col justify-center bg-gray-500 bg-opacity-50 rounded-3xl'>
-
-      <div>
-      <div className=' font-bold text-3xl p-3 '>Convert Text to Sign Language</div>
-      <a href={domain} rel="noopener noreferrer">        
-        <button className='p-4 m-2 bg-amber-400 rounded-2xl p-4 ml-[130px] w-44 border-2 border-black font-semibold text-xl  hover:bg-amber-300'>Convert</button>
-      </a>
+      {/* <img className='relative opacity-20' src='https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg' /> */}
+      <div style={{
+        margin: 0,
+        padding: 0,
+        overflowX: 'hidden',
+      }}>
+        <div className="big-image" style={{
+          height: '100vh',
+          width: '100%',
+          backgroundSize: 'cover',
+          backgroundPosition: '50% 50%',
+          backgroundImage: 'linear-gradient(rgba(255,255,255,0.3), rgba(255,255,255,0.3)), url("https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg")'
+          // backgroundImage: 'url(https://w.forfun.com/fetch/2c/2ceda46989c2534b90b8aabf48f66a97.jpeg)',
+        }}>
+          <div className="text-stone-200" style={{
+            position: 'absolute',
+            top: '10%',
+            left: 0,
+            height: '90%',
+            width: '100%',
+            backgroundColor: 'rgba(0,0,0,0.05)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}>
+          <div className="bg-zinc-950 bg-opacity-95 p-10 rounded-lg">
+            <h1 style={{
+              textAlign: 'center',
+              fontSize: '65px',
+              margin: '0 0 5px',
+              letterSpacing: '0',
+              fontWeight: 700,
+            }}>Welcome to SignLingual!</h1>
+            <p style={{
+              fontSize: '30px',
+              margin: 0,
+            }}>Break down communication barriers, Translate text into sign language instantly!</p>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              marginTop: '20px',
+            }}>
+              <Link to={domain}>
+              <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              >Convert</button>
+              </Link>
+              <Link to="/login">
+                <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2">Learn</button>  
+              </Link>
+            </div>
+          </div>
+          </div>
+        </div>
       </div>
 
-      <div className=' mt-10'>
-        <div className=' font-bold text-3xl p-3  ml-[50px]'>Login to Start Learning</div>
-        <Link to= "/login">
-        <button className='p-4 m-2 bg-amber-400 rounded-2xl p-4 ml-[130px] w-44 border-2 border-black font-semibold text-xl hover:bg-amber-300'>Login</button>
-        </Link>
+      {/* <div className="flex justify-center items-center h-screen">
+        <div className="relative opacity-100 p-32 flex flex-col justify-center bg-gray-500 bg-opacity-50 rounded-3xl">
+          <div>
+            <div className=" font-bold text-3xl p-3 ">
+              Convert Text to Sign Language
+            </div>
+            <a href={domain} rel="noopener noreferrer">
+              <button className="p-4 m-2 bg-amber-400 rounded-2xl p-4 ml-[130px] w-44 border-2 border-black font-semibold text-xl  hover:bg-amber-300">
+                Convert
+              </button>
+            </a>
           </div>
 
-      </div>
-    </div>
-    
-    
-    
+          <div className=" mt-10">
+            <div className=" font-bold text-3xl p-3  ml-[50px]">
+              Login to Start Learning
+            </div>
+            <Link to="/login">
+              <button className="p-4 m-2 bg-amber-400 rounded-2xl p-4 ml-[130px] w-44 border-2 border-black font-semibold text-xl hover:bg-amber-300">
+                Login
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div> */}
     </div>
     // </div>
-    );
+  );
   };
   
   export default Landing;
